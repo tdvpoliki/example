e35f84d53119a663b1c7bd9287958a331603dcb9
tdvpoliki <tdvpoliki@gmail.com>
2022-04-05 22:19:23 +0300

    изменения

--- mail.php
+++ mail.php
@@ -20,7 +20,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('iva.nov.ivan@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('tdvpoliki@gmail.com');     // Кому будет уходить письмо 
+$mail->addAddress('natanivanat@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
